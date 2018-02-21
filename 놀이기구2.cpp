9eb19e3124c66c657e491b0e906b6bd689ca0091
iceCreamParlor <heejjeeh@gmail.com>
2018-02-21 18:25:43 +0900

    feb21

--- 놀이기구2.cpp
+++ 놀이기구2.cpp
@@ -1,8 +1,42 @@
 #include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#define MAX 10001
 
+using namespace std;
+typedef long long ll;
+ll sch[MAX];
+ll n, m, mid, cnt=6;
+
+ll compute(ll time){
+	ll result=0;
+	for(int i=1 ; i<= m ; i++){
+		result += (time / sch[i]);
+	}
+	return result;
+}
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	
+	cin >> n >> m;
+	for(int i=1 ; i<= m ; i++)
+		scanf("%d", &sch[i]);
+	if(n <= 5)	{cout << n ; exit(1);} 
+	
+	ll left =1, right = 2000000001;
+	ll mini= MAX;
+	while(left <= right){
+		mid = (left + right)/2;
+		if(compute(mid) <= n){
+			left = mid +1;
+			if(mini > mid) mini = mid; 
+		}
+		else if(compute(mid) > n){
+			right = mid -1;
+		}
+		
+	}
+	cout << mid;
 	return 0;
 }
---
+++ 사다리.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	double x, y, c;
+	while(scanf("%lf %lf %lf", &x, &y, &c) ==3){
+		double left=0; double right = min(x, y);
+		while(abs(right- left)> 1e-6){
+			double mid= ( left+ right)/ 2.0;
+			double d= mid;
+			double h1= sqrt(x*x- d*d);
+			double h2= sqrt(y*y- d*d);
+			double h= (h1* h2)/ (h1+h2);
+			if(h> c){
+				left = mid;
+			}
+			else{
+				right =mid;
+			}
+		}
+		printf("%.3lf\n", right);
+	}
+	return 0;
+}
